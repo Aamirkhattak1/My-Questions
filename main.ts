6fde20abb946c60e0fad65fda13bcd667e9e0472
Aamirkhattak1 <162826445+Aamirkhattak1@users.noreply.github.com>
2024-03-09 22:38:02 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,15 @@
+
+// QUESTION 1 
+console.log("Aamir Khattak")
+console.log("Babar Azam")
+console.log("Iftikhar")
+console.log("HELLO WORLD");
+
+//QUESTION 2 
+//
+//let myName = 'Aamir khattak':
+//let message = 'is a good student':
+// Console.log(myName+" " +message)
+let myName = 'Aamir khattak';
+let message = 'is a very dominant man';
+console.log (myName+' '+message);
